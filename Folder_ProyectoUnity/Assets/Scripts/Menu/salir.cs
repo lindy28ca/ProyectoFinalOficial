dd204e1f5c98953bc12b3f1c4bd946d8b0c93b1a
lindy28ca <aliagagreis@gmail.com>
2024-12-08 23:54:14 -0500

    S

---
+++ Folder_ProyectoUnity/Assets/Scripts/Menu/salir.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class salir : MonoBehaviour
+{
+public void sal()
+    {
+        Application.Quit();
+    }
+}
